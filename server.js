d9dd3335fc52b8a53fffb28a237c8542309a049a
Jose <jflopez.dev@gmail.com>
2018-07-24 00:33:32 -0400

    Descubierto el error y comentado

--- server.js
+++ server.js
@@ -2,13 +2,23 @@ const express = require('express')
 const hbs = require('hbs')
 const fs = require('fs')
 
-var app = express();
-//use es middleware aunque sirve el html
-//send imprime
+//variable con objeto que tiene variables de entorno
+//if port no existe, usamos el 3000
+const port = 3000
+
+var app = express()
+//"use" toma una funcion que sea middleware
+//send imprime directamente en el DOM
 //applist el segundo argumento es opcional
 //hbs template engine
-
+//Se necesita usar use con la ruta completa __dirname
+//Al declarar el express.static podemos llamar los archivos /help.html
+//por ejemplo, directamente, localizado dentro de la carpeta publica
 hbs.registerPartials(__dirname+'/views/partials')
+//app.set llama a view engine y define que hbs será quien maneje el engine
+//template de la aplicacion
+//hbs se trabaja con html mustache
+//las vistas del template estan dentro del view folder
 app.set('view engine','hbs')
 app.use(express.static(__dirname+'/public'))
 
@@ -23,14 +33,19 @@ app.use((req,res,next)=>{
       console.log('Imposible agregar al archivo server.log')
     }
   })
-  res.render('maintenance.hbs',{
-    pageTitle: 'Mantenimiento',
-    welcomeMsj: 'Volveremos pronto!'
-  })
+  //Este middleware si se desconecta se antepone al contenido real,
+  //pero al llamar partials dentro de su hbs genera conflicto con el hbs
+  //del llamado en el request
+  // res.render('maintenance.hbs',{
+  //   pageTitle: 'Mantenimiento',
+  //   welcomeMsj: 'Volveremos pronto!'
+  // })
   next()
 })
 
-
+//helpers para usar en otro lado, es simplemente una funcion, lo que pueden
+//ser multiples funciones, el primer parametro es el nombre de la funcion
+//Estos se pueden llamar directo desde el hbs con el nombre "{{getCurrentYear}}"
 hbs.registerHelper('getCurrentYear',()=>{
   return new Date().getFullYear()
 })
@@ -56,8 +71,15 @@ app.get('/',(req,res)=>{
 
 app.get('/about',(req,res)=>{
   // res.send('About Page')
+  //app.render se encarga de renderizar el archivo hbs
   res.render('about.hbs',{
     pageTitle: 'About'
+  })
+})
+
+app.get('/test',(req,res)=>{
+  res.render('test.hbs',{
+    pageTitle: 'tesT'
   })
 })
 
@@ -67,6 +89,6 @@ app.get('/bad',(req,res)=>{
   })
 })
 
-app.listen(8080,()=>{
+app.listen(port,()=>{
   console.log('Server is Up')
 })
